b205cffdad8a7b8df7f8799917d707d03ea66ef2
mjcross8 <mekhicrossland@students.berkeley.net>
2021-03-11 12:54:09 -0800

    Attempt at back driver wheel control

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,12 +1,18 @@
 
-
+#include "chassis.h"
 #include "main.h"
-// Mr Wright Was Here
-// Mekhi is here
-//James was here
+
 void operatorControl() {
+	int loopCount = 0; //just a loop counter
+	int power, turn;
+
 	while (1) {
-		printf("Hello PROS User!\n");
-		delay(20);
+
+		power = joystickGetAnalog(1, 3); // vertical axis on left joystick
+		turn  = joystickGetAnalog(1, 3); // vertical axis on left joystick
+		chassisSet(power + turn, turn - power);
+
+
+		loopCount = loopCount + 1 ;
 	}
 }
