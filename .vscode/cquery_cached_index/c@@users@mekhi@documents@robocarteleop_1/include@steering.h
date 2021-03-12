2aea20fc28e10b46ab098b1b784d1905059a99e5
mjcross8 <mekhicrossland@students.berkeley.net>
2021-03-12 12:25:13 -0800

    Commit

---
+++ .vscode/cquery_cached_index/c@@users@mekhi@documents@robocarteleop_1/include@steering.h
@@ -0,0 +1,22 @@
+#ifndef _STEERING_H_
+#define _STEERING_H_
+
+#include "main.h"
+
+
+int Kp = 1;
+
+
+void steeringSet(int speed)
+{ motorSet(3, speed); }
+
+
+void elbowHold(int elbowTarget){
+
+ printf("The steering encoder value is %d\n", encoderGet(steeringEncoder));
+ int counts = encoderGet(steeringEncoder);
+ int error = (joystickGetAnalog(1, 1) - counts);
+ steeringSet(Kp * error);
+ }
+
+#endif
--- .vscode/cquery_cached_index/c@@users@mekhi@documents@robocarteleop_1/src@opcontrol.c
+++ .vscode/cquery_cached_index/c@@users@mekhi@documents@robocarteleop_1/src@opcontrol.c
@@ -6,6 +6,10 @@
 //James was here
 void operatorControl() {
 	while (1) {
+
+		if(joystickGetAnalog(1, 1)){
+			steeringSet(50);
+		}
 		printf("Hello PROS User!\n");
 		delay(20);
 	}
