1f950bbc43eda3b348491aa75d1162d77ba08035
mjcross8 <mekhicrossland@students.berkeley.net>
2021-03-12 12:58:51 -0800

    Cleaning up errors

--- src/init.c
+++ src/init.c
@@ -7,5 +7,5 @@ void initializeIO() {
 
 
 void initialize() {
-  steeringEncoder = encoderInit(1, 2, TRUE);
+  steeringEncoder = encoderInit(1, 2, true);
 }
--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,27 +1,22 @@
 
-#include "chassis.h"
 #include "main.h"
+#include "chassis.h"
+#include "steering.h"
 
 void operatorControl() {
-	while (1) {
 
 		int loopCount = 0; //just a loop counter
 		int power, turn;
 
 		while (1) {
 
-			power = joystickGetAnalog(1, 3); // vertical axis on left joystick
-			turn  = joystickGetAnalog(1, 3); // vertical axis on left joystick
-			chassisSet(power + turn, turn - power);
+			power = joystickGetAnalog(1, 3);
+			chassisSet(power, -power);
 
-		if(joystickGetAnalog(1, 1)){
-			steeringSet(50);
-		}
-		printf("Hello PROS User!\n");
-		delay(20);
+
+			steeringSet(joystickGetAnalog(1, 1));
 
 
 		loopCount = loopCount + 1 ;
 	}
 	}
-}
