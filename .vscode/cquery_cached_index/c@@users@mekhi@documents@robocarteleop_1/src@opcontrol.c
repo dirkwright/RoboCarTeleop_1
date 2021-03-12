63fe16abb34e1ac925e6b4dc5a6fe9eb16c070a1
mjcross8 <mekhicrossland@students.berkeley.net>
2021-03-12 12:33:48 -0800

    Added chassis to op control

--- .vscode/cquery_cached_index/c@@users@mekhi@documents@robocarteleop_1/src@opcontrol.c
+++ .vscode/cquery_cached_index/c@@users@mekhi@documents@robocarteleop_1/src@opcontrol.c
@@ -1,16 +1,27 @@
 
-
+#include "chassis.h"
 #include "main.h"
-// Mr Wright Was Here
-// Mekhi is here
-//James was here
+
 void operatorControl() {
 	while (1) {
+
+		int loopCount = 0; //just a loop counter
+		int power, turn;
+
+		while (1) {
+
+			power = joystickGetAnalog(1, 3); // vertical axis on left joystick
+			turn  = joystickGetAnalog(1, 3); // vertical axis on left joystick
+			chassisSet(power + turn, turn - power);
 
 		if(joystickGetAnalog(1, 1)){
 			steeringSet(50);
 		}
 		printf("Hello PROS User!\n");
 		delay(20);
+
+
+		loopCount = loopCount + 1 ;
+	}
 	}
 }
