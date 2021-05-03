cbd830c73b92dce2480fc33d168581f8576b70d6
mjcross8 <mekhicrossland@students.berkeley.net>
2021-05-03 11:35:44 -0700

    Autonomous

--- src/init.c
+++ src/init.c
@@ -3,10 +3,15 @@
 
 
 void initializeIO() {
+
+
 }
+
 
 
 void initialize() {
   steeringEncoder = encoderInit(1, 2, true);
   driveEncoder = encoderInit(3, 4, true);
+
+wallSensor =ultrasonicInit (7,8);
 }
